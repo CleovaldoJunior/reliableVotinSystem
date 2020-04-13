68294360a96b651a5a563f1d150655e6b960a40c
CleovaldoJunior <cleovaldobcc@gmail.com>
2020-04-13 07:32:18 -0300

    correção consultas no banco

--- Banco.py
+++ Banco.py
@@ -3,9 +3,22 @@ import pymysql
 cone = pymysql.connect(host="localhost", user="root", passwd="", database="votingsystembd")
 cursor = cone.cursor()
 
-
-def inserir_eleitor_banco(eleitor):
-    args = (eleitor.get_nome(), eleitor.get_idade(), eleitor.get_cpf(), eleitor.get_sexo(), eleitor.get_titulo())
-    cursor.execute("INSERT INTO eleitor(nome, idade, cpf, sexo, titulo) VALUES (%s, %s, %s, %s, %s)", args)
+def commit():
     cone.commit()
 
+def inserir_eleitor_bd(eleitor):
+    args = (eleitor.get_nome(), eleitor.get_idade(), eleitor.get_cpf(), eleitor.get_sexo(), eleitor.get_titulo())
+    cursor.execute("INSERT IGNORE INTO eleitor(nome, idade, cpf, sexo, titulo) VALUES (%s, %s, %s, %s, %s)", args)
+    cursor.execute('SELECT 1 FROM eleitor e WHERE e.cpf= %s', args[2])
+    if not cursor.fetchone():
+        cursor.execute("INSERT INTO eleitor(nome, idade, cpf, sexo, titulo) VALUES (%s, %s, %s, %s, %s)", args)
+    commit()
+
+def deletar_eleitor_bd(cpf):
+    cursor.execute("DELETE FROM eleitor WHERE cpf = %s", cpf)
+    commit()
+
+def mostrar_eleitores_bd():
+    cursor.execute("SELECT * FROM `eleitor`")
+    for i in cursor:
+        print(i)
